fa957ceb6e7c5b4e956162eefe3e74277010028f
fortalesi <75318821+fortalesi@users.noreply.github.com>
2022-01-14 12:07:23 +0200

    updated an import

--- src/components/common/GuardedRoute.js
+++ src/components/common/GuardedRoute.js
@@ -1,4 +1,4 @@
-import  { Navigate, Nvaigate, Outlet } from 'react-router-dom';
+import  { Navigate, Outlet } from 'react-router-dom';
 import { useAuthContext } from  '../../contexts/AuthContext';
 
 const GuardedRoute = ()=> {
